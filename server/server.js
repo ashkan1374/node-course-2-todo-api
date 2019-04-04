f21f6d75b6551eaf88445600d12d2170cb67ddaf
ashkan <haghgo1374.ashkan@gmail.com>
2019-04-04 17:45:46 +0430

    'todo'

---
+++ server/server.js
@@ -0,0 +1,58 @@
+/*
+
+
+let newTodo=new Todo({
+    text: "Cook Dinner"
+});
+
+newTodo.save().then((doc)=>{
+    console.log(`Saved todo : ${doc}`);
+},(err)=>{
+    console.log('Unable to save todo');
+});
+
+
+
+let newUser = new User({
+    name: 'ashkan',
+    email: 'ashkan1374@mail.com'
+});
+
+newUser.save().then((doc) => {
+    console.log(`Saved user : ${doc}`);
+}, (err) => {
+    console.log('Unable to save user');
+});
+
+
+*/
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const {mongoose}=require('./db/mongoose');
+const {Todo}=require('./models/todo');
+const {User}=require('./models/user');
+
+const app=express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    let todo=new Todo({
+        text:req.body.text
+    });
+
+    todo.save().then((doc)=>{
+       res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('App is Running on Port 3000');
+});
+
+
+
+module.exports={app};
