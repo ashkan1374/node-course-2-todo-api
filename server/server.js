99a9615252c5b33ef542de404b7b969267a0b802
ashkan <haghgo1374.ashkan@gmail.com>
2019-04-08 17:39:52 +0430

    node test todo with id

--- server/server.js
+++ server/server.js
@@ -59,18 +59,21 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-    let id = req.params.id;
+    var id = req.params.id;
+
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
-    Todo.findById(id).then((todo) => {
-        res.status(200).send(todo);
-    }, (err) => {
-        res.status(404).send({});
-    }).catch(e => {
-        res.status(400).send(e);
-    })
 
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 if (!module.parent) {
