6301d19c72ed4264128bad9ff85aa869583bb453
ashkan <haghgo1374.ashkan@gmail.com>
2019-04-25 17:00:51 +0430

    delete /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,17 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    let user = this;
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function (token) {
     let User = this;
     let decoded;
@@ -73,6 +84,25 @@ UserSchema.statics.findByToken = function (token) {
         '_id': decoded._id,
         'tokens.token': token,
         'tokens.access': 'auth'
+    });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    let User = this;
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject(err);
+                }
+
+            });
+        });
     });
 };
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,7 +4,7 @@ const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
-const {User} =require('./../models/user');
+const {User} = require('./../models/user');
 beforeEach(populateUsers);
 beforeEach(populateTodos);
 
@@ -194,15 +194,15 @@ describe('POST /users', () => {
                 expect(res.headers['x-auth']).toExist();
                 expect(res.body._id).toExist();
                 expect(res.body.email).toBe(email);
-            }).end((err)=>{
-                if (err){
-                    return done(err);
-                }
-                User.findOne({email}).then((user)=>{
-                    expect(user).toExist();
-                    expect(user.password).toNotBe(password);
-                    done();
-                });
+            }).end((err) => {
+            if (err) {
+                return done(err);
+            }
+            User.findOne({email}).then((user) => {
+                expect(user).toExist();
+                expect(user.password).toNotBe(password);
+                done();
+            }).catch((e) => done(e));
         });
     });
 
@@ -210,21 +210,86 @@ describe('POST /users', () => {
         request(app)
             .post('/users')
             .send({
-                email:'and',
-                password:'123'
+                email: 'and',
+                password: '123'
             })
             .expect(400)
             .end(done);
     });
 
-    it('should not create user if email in use', (done)=> {
+    it('should not create user if email in use', (done) => {
 
         request(app)
             .post('/users')
             .send({
-                email:users[0].email,
-                password:'1234'})
+                email: users[0].email,
+                password: '1234'
+            })
             .expect(400)
             .end(done);
+    });
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            }).expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            }).end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).toInclude({
+                    access: 'auth',
+                    token: res.headers['x-auth']
+                });
+                done();
+            }).catch((e) => done(e));
+        })
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + '1'
+            }).expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            })
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e=>done(e));
+            });
     });
 });
